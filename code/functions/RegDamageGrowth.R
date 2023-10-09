dd04d62ef432d530a7629f249208e440ce36502c
JiaruiZhongPik <127195273+JiaruiZhongPik@users.noreply.github.com>
2023-10-09 11:59:53 +0200

    Correct the fixed effect in the regression function

--- code/functions/RegDamageGrowth.R
+++ code/functions/RegDamageGrowth.R
@@ -30,7 +30,7 @@ RegDamageGrowth <- function(data,model){
     
   #Complete the regression function by adding fixed effects.  
   formula_str <- paste("Growth_income ~ ", paste(alist, collapse = " + "),
-                        "+ as.numeric(Year) : state  + panelid + state")  
+                        "+ as.numeric(Year) : state  + panelid + as.numeric(Year)")  
 
   
 
